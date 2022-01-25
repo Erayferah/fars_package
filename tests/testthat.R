aeb329b98c313c9af0d958a8dfdd6a5a3501015c
Eray Ferah <ery.ferah@gmail.com>
2022-01-25 22:00:25 +0100

    tests included vignette initiated.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,12 @@
 library(testthat)
-library(CourseraRDev)
 
-test_check("CourseraRDev")
+make_filename <- function(year) {
+  year <- as.integer(year)
+  sprintf("accident_%d.csv.bz2", year)
+}
 
-
+test_that("fars make_filename function tests", {
+  testthat::expect_that(make_filename(2013), testthat::equals("accident_2013.csv.bz2"))
+  testthat::expect_that(make_filename(2014), testthat::equals("accident_2014.csv.bz2"))
+  testthat::expect_that(make_filename(2015), testthat::equals("accident_2015.csv.bz2"))
+})
